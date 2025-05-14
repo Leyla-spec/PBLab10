340659abe17623781235511d4551c1b733b9f61d
Leyla-spec <leylasa@code.edu.az>
2025-05-14 17:47:46 +0400

    Added Lab10

---
+++ PBLab10/PB305Lab10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PB305Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User user = new User("Ali", 25, "paSsword");
+            Console.WriteLine(user.Password);
+            //Person person = new Person("ali", "aliyev", 25);
+            //Console.WriteLine(person.Surname1);
+        }
+    }
+}
